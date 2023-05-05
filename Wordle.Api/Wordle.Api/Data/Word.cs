1a4cc84a7a8c6d51255374f21e169163fd28974c
Tom Rohr <trohr92@live.com>
2023-05-04 17:00:41 -0700

    Class defining Entity generated table members

---
+++ Wordle.Api/Wordle.Api/Data/Word.cs
@@ -0,0 +1,11 @@
+﻿namespace Wordle.Api.Data;
+
+public class Word
+{
+    //Good practice to name ID primary keys as 'ClassName'Id like we did here
+    public int WordId { get; set; }
+    //required modifer used to make sure the property is not null.
+    //It is necessary for Entity Framework to work properly.
+    public required string Text { get; set; }
+    public bool IsCommon { get; set; }
+}
